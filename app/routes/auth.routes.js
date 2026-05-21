2c5ca8e1d98686094c6043a746a2c483fb3b7b27
kitjsr <91665868+kitjsr@users.noreply.github.com>
2026-05-21 20:11:00 +0530

    Update auth.routes.js

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -24,4 +24,6 @@ module.exports = function(app) {
   app.post("/api/auth/signout", controller.signout);
   app.get("/api/auth/users", controller.findAll);
   app.get("/api/auth/usercount", controller.getUserCount);
+  app.get("/api/auth/changeStatus", controller.changeStatus);
+  //
 };
