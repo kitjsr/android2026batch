f820a5c21fa5ddd12725498785eb7d088c87eefb
kitjsr <91665868+kitjsr@users.noreply.github.com>
2026-06-15 19:50:03 +0530

    Update enrollment.routes.js

--- app/routes/enrollment.routes.js
+++ app/routes/enrollment.routes.js
@@ -1,21 +1,17 @@
 module.exports = (app) => {
   const enrollment = require("../controllers/enrollment.controller");
-
   const router = require("express").Router();
 
   router.post("/", enrollment.create);
 
   router.put("/:id/success", enrollment.paymentSuccess);
-
   router.put("/:id/failed", enrollment.paymentFailed);
 
-  router.get("/", enrollment.findAll);
-  
   router.get("/popular", enrollment.popular);
-
-  router.get("/:id", enrollment.findOne);
-
   router.get("/user/:userId", enrollment.findByUser);
+
+  router.get("/", enrollment.findAll);
+  router.get("/:id", enrollment.findOne);
 
   router.put("/:id/progress", enrollment.updateProgress);
 
